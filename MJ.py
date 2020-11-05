ebe610c520c99150c88dee2b24c809118ef1bace
sai <saichandra123981@gmail.com>
2020-11-05 10:09:12 +0530

    first commit

---
+++ MJ.py
@@ -0,0 +1,15 @@
+WIDTH=40
+NUM_BOMBS=10
+class Tile:
+    def __init__(self):
+        self.bomb=False
+grid=[[Tile() for n in range(9)] for n in range(9)]
+
+for n in range(NUM_BOMBS):
+    
+
+def setup():
+    size(800,600)
+
+def draw():
+    pass
